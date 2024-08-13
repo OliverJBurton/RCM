3b2210738bbec151d23062c698b0b023ce16cf53
Imafont <0220161718@harrowschool.hk>
2024-08-13 16:54:37 +0100

    Made code more readable

---
+++ RCM/RCM.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+
+from camera import Camera_HS #Spectral
+from camera import Camera_BA #RGB
+from light import DC2200
+from stage import Controller
+from tunablefilter import TunableFilter
+import time
+import os
+import cv2
+import imageio
+
+
+class FullControlMicroscope:
+    def __init__(self, wavelength_range=[420, 730], no_spectra=5, exposure_time=0.0, save_folder=""):
+        self.wavelength_range = wavelength_range
+        self.no_spectra = no_spectra
+        self.exposure_time = exposure_time
+        self.save_folder = save_folder
+
+        # initialising all sections:
+        self.chs = Camera_HS()
+        print('HS camera connected')
+        self.cba = Camera_BA()
+        print('TL camera connected')
+        self.led = DC2200()
+        print('LS connected')
+        self.lcf = TunableFilter()
+        print('LC connecting...')
+        self.lcf.open()
+        print('LC connected')
+
+        self.sta = Controller(which_port='COM4',
+                              stages=('ZFM2030', 'ZFM2030', 'ZFM2030'),
+                              reverse=(False, False, True),
+                              verbose=True,
+                              very_verbose=False)
+        print('stage connected')
+
+        self.stage_check()
+
+    def stage_check(self):
+        print("Please ensure the position values of each motor are within their respective limits. If not, move the stage back to it's unextended position to recalibrate its zero position. Also do this if the zero position of the stage has been altered on the MCM3000 Microscopy Controllers Software.")
+        while True:
+            response = input("Does the zero position of the stage requiring recalibrating? Y/N")
+            if response == "N":
+                break
+            elif response == "Y":
+                for channel in self.sta.channels:
+                    self.sta._set_encoder_counts_to_zero(channel)
+                print("Completed recalibration")
+                break
+
+    def close(self):
+        '''
+        Closes all peripherals
+        :return:
+        '''
+        self.chs.close()
+        self.cba.close()
+        self.led.close()
+        self.sta.close()
+        self.lcf.close()
+
+    def save_image(self, wavelengths, hypercube, indices):
+        '''
+        Saves image in save_folder
+        :param wavelengths: list of wavelengths in nm
+        :param hypercube: list of 3D images
+        :param indices: list of identifiers to append to the name of each image
+        '''
+        for ii, wl in enumerate(wavelengths):
+            image_name = 'image_cap_' + str(ii) + '_' + str(wl) + '_' + '_'.join(str(e) for e in indices) + '_img.png'
+            fn = os.path.join(self.save_folder, image_name)
+            imageio.imwrite(fn, hypercube[ii].astype(np.uint16))     
+
+    def aquire_HS_datacube(self):
+        '''
+        Takes images filtered at a range of wavelengths
+        :param wavelength_range: range in nm of wavelengths
+        :param no_spectra: number of spectral points
+        :param exposuretime: exposure time of camera
+        :param save_folder: empty list or name of folder
+        :return: (list of wavelengths in nm, list of 3D images)
+        '''
+
+        # Capturing images with multiple exposures and storing the mean image in 'capture'
+        wavelengths = np.linspace(self.wavelength_range[0], self.wavelength_range[1], self.no_spectra)
+        hypercube = []
+
+        for wl in wavelengths:
+            self.lcf.set_wavelength(int(wl))
+            time.sleep(3e-2)
+            hypercube.append(self.chs.single_exposure(exposure_time=self.exposure_time))
+
+        return wavelengths, hypercube
+
+    def take_HS_time_series_images(self, time_increment=10, total_time=7200):
+        '''
+        Does a hyperspectral imaging over time and saves the images
+        :param time_increment: time between measurements
+        :param total_time: time for all data measurements
+        '''
+        t0 = time.time()
+        while time.time() - t0 < total_time:
+            time.sleep(time_increment)
+            wavelengths, hypercube = self.aquire_HS_datacube()
+            self.save_image(wavelengths, hypercube, [time.time() - t0])            
+
+    def mapping(self, sample_dim=[], sample_no_per_channel=[], RGB_img_too=False):
+        '''
+        Performs mapping based off size of each sample or the number of samples or both and saves image. Keep sample_no_per_channel as an empty list if you want to perform mapping based off sample size and vice versa. 
+        :param: sample_dim: [dimensions of sample along channel 0, dimensions of sample along channel 1]
+        :param: sample_no_per_channel: [number of samples along channel 0, number of samples along channel 1]
+        :param: RGB_img_too: takes RGB image as well as hyperspectral image if True
+        '''
+
+        # Either create list of locations based off size of each sample or the number of samples or both
+        positions_list = []
+        if sample_dim != [] and sample_no_per_channel != []:
+            for c in self.sta.channels[:2]:
+                # Starting from lower limit, generate required number of boxes unless coordinate of the box gets too close to upper limit
+                positions_list.append([-self.sta._position_limit_um[c]+sample_dim[c]*(i+1/2) for i in range(sample_no_per_channel[c]) if -self.sta._position_limit_um[c]+sample_dim[c]*(i+1/2) <= self.sta._position_limit_um[c]-sample_dim[c]])
+        elif sample_dim != []:
+            for c in self.sta.channels[:2]:
+                # Positions of stage, camera must be centered on the box
+                positions_list.append(np.arange(-self.sta._position_limit_um[c]+sample_dim[c]/2, self.sta._position_limit_um[c]-sample_dim[c]/2, sample_dim[c]))
+        elif sample_no_per_channel != []:
+            for c in self.sta.channels[:2]:
+                # Determine shift required to get from side of each box to the center of each box
+                shift = self.sta._position_limit_um[c]/sample_no_per_channel[c]
+
+                # Determine position of the corner of each box, add shift to move position to center of box
+                positions_list.append(np.linspace(-self.sta._position_limit_um[c], self.sta._position_limit_um[c], sample_no_per_channel[c], endpoint=False) + shift)
+        else:
+            print("Enter valid parameters")
+            return None
+
+        # iterate throughout all possible boxes
+        for i in positions_list[0]:
+            self.sta.move_um(self.sta.channels[0], i, relative=False)
+            for j in positions_list[1]:
+                self.sta.move_um(self.sta.channels[1], j, relative=False)
+
+                if RGB_img_too:
+                    pool = Pool()
+                    spectral = pool.apply_async(self.aquire_HS_datacube, [wavelength_range, no_spectra, exposure_time])
+                    rgb = pool.apply_async(self.cba.average_exposure, [exposure_time])
+
+                    wavelengths, hypercube = spectral.get()
+                    rgbImage = rgb.get()
+                    self.save_image(wavelengths, hypercube, save_folder, [i, j])
+                    imageio.imwrite(fn, rgbImage)
+
+                # Take hyperspectral imaging of each box
+                wavelengths, hypercube = self.aquire_HS_datacube()
+                self.save_image(wavelengths, hypercube, [i, j])
--- main.py
+++ main.py
@@ -1,182 +1,18 @@
 import sys
 
 sys.path.append('../RCM/RCM')
-import numpy as np
-import matplotlib.pyplot as plt
-from multiprocessing import Pool
 
-from camera import Camera_HS #Spectral
-from camera import Camera_BA #RGB
-from light import DC2200
-from stage import Controller
-from tunablefilter import TunableFilter
-import time
-import os
-import cv2
-import imageio
-
-
-class FullControlMicroscope:
-    def __init__(self):
-        # initialising all sections:
-        self.chs = Camera_HS()
-        print('HS camera connected')
-        self.cba = Camera_BA()
-        print('TL camera connected')
-        self.led = DC2200()
-        print('LS connected')
-        self.sta = Controller(which_port='COM4',
-                              stages=('ZFM2030', 'ZFM2030', 'ZFM2030'),
-                              reverse=(False, False, True),
-                              verbose=True,
-                              very_verbose=False)
-        print('stage connected')
-
-        self.lcf = TunableFilter()
-        print('LC connecting...')
-
-        self.lcf.open()
-        print('LC connected')
-
-    def close(self):
-        '''
-        Closes all peripherals
-        :return:
-        '''
-        self.chs.close()
-        self.cba.close()
-        self.led.close()
-        self.sta.close()
-        self.lcf.close()
-
-    def save_image(self, wavelengths, hypercube, save_folder, data):
-        for ii, wl in enumerate(wavelengths):
-            image_name = 'image_cap_' + str(ii) + '_' + str(wl) + '_' + '_'.join(str(e) for e in data) + '_img.png'
-            print(image_name)
-            fn = os.path.join(save_folder, image_name)
-            imageio.imwrite(fn, hypercube[ii].astype(np.uint16))     
-
-    def aquire_HS_datacube(self, wavelength_range=[420, 730], no_spectra=5, exposuretime=0.0, save_folder=""):
-        '''
-
-        :param wavelength_range: range in nm of wavelengths
-        :param no_spectra: number of spectral points
-        :param exposuretime: exposure time of camera
-        :param save_folder: empty list or name of folder
-        :return wavelengths: 1D list of wavelengths
-        :return hypercube: list of 3D images
-        '''
-
-        if exposuretime == 0.0:
-            exposure_time = self.chs.exposure
-        else:
-            exposure_time = exposuretime
-
-        # Capturing images with multiple exposures and storing the mean image in 'capture'
-        wavelengths = np.linspace(wavelength_range[0], wavelength_range[1], no_spectra)
-        hypercube = []
-
-        for wl in wavelengths:
-            self.lcf.set_wavelength(int(wl))
-            time.sleep(3e-2)
-            hypercube.append(self.chs.single_exposure(exposure_time=exposure_time))
-
-        if save_folder == "":
-            return wavelengths, hypercube
-        else:
-            self.save_image(wavelengths, hypercube, save_folder, [])
-
-    def aquire_HS_time_series(self, wavelength_range=[420, 730], no_spectra=5, exposuretime=0.0,
-                              save_folder="", time_increment=10, total_time=7200):
-        '''
-        :param time_increment:
-        :param total_time:
-        :return:
-        '''
-        t0 = time.time()
-        while time.time() - t0 < total_time:
-            time.sleep(time_increment)
-            wavelengths, hypercube = self.aquire_HS_datacube(wavelength_range=wavelength_range, no_spectra=no_spectra, exposuretime=exposuretime)
-            self.save_image(wavelengths, hypercube, save_folder, [time.time() - t0])            
-
-    def mapping(self, channels=[0,1], sample_dim=[], sample_no_per_channel=[], wavelength_range=[420, 730], no_spectra=5, save_folder="", RGB_img_too=False, exposure_time=0):
-        """
-        Steps
-        0. Check if channel is available
-        1. Move stage to initial position
-        Loop
-        2. Get reading
-        3. Move to next position
-        EndLoop
-        4. Save reading
-        """
-
-        # Validation checks
-        assert channels[0] == 0 and channels[1] == 1, ("Invalid channel")
-        # assert (sample_dim[0] != 0 and sample_dim[1] != 0), ("Dimensions of sample cannot be 0")
-        # assert (sample_no_per_channel[0] > 0 and sample_dim[1] > 0), ("Number of samples along each channel cannot be 0 or negative")
-        # assert (type(sample_no_per_channel[0]) == type(1) and type(sample_no_per_channel[1]) == type(1)), ("Number of samples along each channel must be an integer")
-
-        # Either create list of locations based off size of each sample or the number of samples or both
-        positions_list = [[],[]]
-        if sample_dim != [] and sample_no_per_channel != []:
-            for c in channels[:2]:
-                # Starting from lower limit, generate required number of boxes unless coordinate of the box gets too close to upper limit
-                positions_list[c] = [-self.sta._position_limit_um[c]+sample_dim[c]*(i+1/2) for i in range(sample_no_per_channel[c]) if -self.sta._position_limit_um[c]+sample_dim[c]*(i+1/2) <= self.sta._position_limit_um[c]-sample_dim[c]]
-        elif sample_dim != []:
-            for c in channels[:2]:
-                # Positions of stage, camera must be centered on the box
-                positions_list[c] = np.arange(-self.sta._position_limit_um[c]+sample_dim[c]/2, self.sta._position_limit_um[c]-sample_dim[c]/2, sample_dim[c])
-        elif sample_no_per_channel != []:
-            for c in channels[:2]:
-                # Determine shift required to get from side of each box to the center of each box
-                shift = self.sta._position_limit_um[c]/sample_no_per_channel[c]
-
-                # Determine position of the corner of each box, add shift to move position to center of box
-                positions_list[c] = np.linspace(-self.sta._position_limit_um[c], self.sta._position_limit_um[c], sample_no_per_channel[c], endpoint=False) + shift
-        else:
-            print("Enter valid parameters")
-            return None
-
-        print(positions_list)
-
-        # iterate throughout all possible boxes
-        for i in positions_list[0]:
-            self.sta.move_um(channels[0], i, relative=False)
-            for j in positions_list[1]:
-                self.sta.move_um(channels[1], j, relative=False)
-
-                # Use camera to take a snapshot of each box
-
-                if RGB_img_too:
-                    pool = Pool()
-                    spectral = pool.apply_async(self.aquire_HS_datacube, [wavelength_range, no_spectra, exposure_time])
-                    rgb = pool.apply_async(self.cba.average_exposure, [exposure_time])
-
-                    wavelengths, hypercube = spectral.get()
-                    rgbImage = rgb.get()
-                    self.save_image(wavelengths, hypercube, save_folder, [i, j])
-                    imageio.imwrite(fn, rgbImage)
-
-                wavelengths, hypercube = self.aquire_HS_datacube(wavelength_range, no_spectra, exposure_time)
-                self.save_image(wavelengths, hypercube, save_folder, [i, j])
-
-
-
+from RCM import FullControlMicroscope
 
 if __name__ == '__main__':
-    if 0:
-        cam = Camera_HS()
-        img = cam.single_exposure(exposure_time=0.654e-3)
-        plt.imshow(img)
-        plt.show()
+    # cam = Camera_HS()
+    # img = cam.single_exposure(exposure_time=0.654e-3)
+    # plt.imshow(img)
+    # plt.show()
 
-    msc = FullControlMicroscope()
-    msc.sta.move_um(channel=0, move_um=0, relative=0)
-    msc.sta.move_um(channel=1, move_um=0, relative=0)
-    msc.sta.move_um(channel=2, move_um=0, relative=0)
+    msc = FullControlMicroscope(exposure_time=0.645e-3, save_folder="C:\\Users\\whw29\\Desktop\\Images")
 
-    msc.mapping(sample_dim=[50, 50], sample_no_per_channel=[1, 1], exposure_time=0.645e-3, save_folder="C:\\Users\\whw29\\Desktop\\Images", RGB_img_too=True)
+    msc.mapping(sample_dim=[50, 50], sample_no_per_channel=[1, 1], RGB_img_too=True)
 
     # # msc.aquire_TL_time_series(time_increment=1,total_time=2,folder=folder,exposure_time=0.654e-3)
     # msc.aquire_HS_datacube(exposuretime=0.654e-3, save_folder="C:\\Users\\whw29\\Desktop\\Images")
