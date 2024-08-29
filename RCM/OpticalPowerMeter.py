19dce3a756db95208786de2cafc86eb88a5702a6
Imafont <0220161718@harrowschool.hk>
2024-08-29 15:12:40 +0100

    Cross your fingies

--- RCM/OpticalPowerMeter.py
+++ RCM/OpticalPowerMeter.py
@@ -1,15 +1,29 @@
 import pyvisa as visa
-from pyThorlabsPM100x.driver import ThorlabsPM100x
 from ThorlabsPM100 import ThorlabsPM100
+import time
 
 #Need to download ThorlabsPM100 module: pip3 install ThorlabsPM100
 
 class PM100:
   def __init__(self):
-    rm = visa.ResourceManager()
-    print(rm.list_resources())
-    inst = rm.open_resource('ASRL5::INSTR', timeout=1)
-    self.power_meter = ThorlabsPM100(inst=inst)
+    self.power_meter = ThorlabsPM100x()
+    print(power_meter.available_devices)
+    self.rm = visa.ResourceManager()
+    
+    inst = self.rm.open_resource('ASRL5::INSTR', 
+    baud_rate=9600, 
+    data_bits=8,
+    parity=Parity.none, 
+    write_termination='\n', 
+    read_termination="\n", 
+    stop_bits=StopBits.one,
+    timeout=1000)
+
+    # inst.write('*IDN?')
+    # while True:
+    #   print(inst.read_bytes(1))
+
+    self.power_meter = ThorlabsPM100(inst = inst, verbose=True)
 
   def read(self):
     return self.power_meter.read
@@ -18,6 +32,5 @@ class PM100:
 if __name__ == "__main__":
   light_reader = PM100()
   print(light_reader.read())
-
 
 
