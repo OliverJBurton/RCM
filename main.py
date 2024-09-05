cd44c6ecddfdd6779d38a973db8c704d598db2c5
Imafont <0220161718@harrowschool.hk>
2024-09-05 14:40:27 +0100

    Added code to test power meter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('../RCM/RCM')
 
 from RCM import FullControlMicroscope
+from OpticalPowerMeter import PM16_120
 
 if __name__ == '__main__':
     # cam = Camera_HS()
@@ -12,7 +13,29 @@ if __name__ == '__main__':
 
     msc = FullControlMicroscope(exposure_time=0.645e-3, save_folder="C:\\Users\\whw29\\Desktop\\Images")
 
-    msc.obtain_intensity_pixel_matrix()
+    power_meter = PM16_120()
+
+    currents = np.linspace(0, 500, 15)
+    powers_mu = []
+
+    for current in currents:
+        msc.led.set_current(current)
+        # Wait for reading to stabilise
+        time.sleep(3)
+
+        powers_mu.append(round(power_meter.get_power_reading_W*10**6), 1)
+    
+    with open("power_PM16.txt", "w+") as file:
+        for power in powers_mu:
+            file.write(f"{power},")
+        file.write("\n")
+    
+    plt.plot(current, powers_mu)
+    plt.xlabel("Current (mA)")
+    plt.ylabel("Power ($\mu$W)")
+    plt.xlim((450, 700))
+    plt.show()
+        
 
     # msc.mapping(sample_dim=[50, 50], sample_no_per_channel=[1, 1], RGB_img_too=True)
 
