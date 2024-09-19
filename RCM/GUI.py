Hopefully fixes scaling?

RCM/GUI.py
@@ -600,12 +600,11 @@ class LightIntensityDetermination:
     min_background_energy = np.min(background_energy_array[:,120:-1])
 
     # Apply rescaling
-    corrected_energy_array = (energy_array - background_energy_array) / (self.f_x_y - background_energy_array) * (min_energy_max - min_background_energy) + min_background_energy
+    corrected_energy_array = (energy_array - background_energy_array) / (self.f_x_y - background_energy_array) * (min_energy_max - min_background_energy)
 
+    # Round then convert to uint8 to prevent overflow errors when converting into RGB image
     corrected_greyscale_array = np.round(np.reshape(self.greyscale_function_of_energy(corrected_energy_array/self.f_x_y), self.exp_screen_res[::-1] +(1,))).astype(np.uint8)
     corrected_rgb_array = np.repeat(corrected_greyscale_array, 3, axis=2)
-    print(corrected_rgb_array.shape)
-
     corrected_image = Image.fromarray(corrected_rgb_array, "RGB")
     corrected_image.show()
     corrected_image_path = f"{image_path.split(".")[0]}_corrected.{image_path.split(".")[1]}"
@@ -619,7 +618,6 @@ class LightIntensityDetermination:
     :returns: numpy array of the padded, rescaled image
 
     """
-
     # Opens image and converts it into greyscale mode
     image = Image.open(image_path).convert("L")
     image_dim = image.size